de92c362cb9ce676f62bf56747b7aa9120f94a0f
Minirazs <ruishan.foo@gmail.com>
2020-09-26 06:01:01 +0000

    added form (new user)

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 import os
 import json
+import random
 
 app = Flask(__name__)
 database = {}
@@ -15,6 +16,34 @@ def home():
 @app.route('/user')
 def show_users():
     return render_template('users.template.html', all_users=database)
+
+@app.route('/user/add')
+def show_add_user():
+    return render_template('add_user.template.html')
+
+@app.route('/user/add', methods=["POST"])
+def process_add_customer():
+    first_name = request.form.get('first_name')
+    last_name = request.form.get('last_name')
+    food = request.form.get('food')
+    calorie = request.form.get('calorie')
+    meal = request.form.get('meal')
+    date = request.form.get('date')
+
+    database.append({
+        'id': random.randint(10000, 99999),
+        'first_name': first_name,
+        'last_name': last_name,
+        'food': food,
+        'calorie': calorie,
+        'meal': meal,
+        'date': date
+    })
+
+    with open('food.json', 'w') as fp:
+        json.dump(database, fp)
+
+    return redirect(url_for('show_users')) #redirect to function's name 'show_users'
 
 
 # "magic code" -- boilerplate
